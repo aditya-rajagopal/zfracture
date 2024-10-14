5b4523cdff429846c9fa2bbdf09be179568a2a9c
Aditya Rajagopal <adi.raj465@gmail.com>
2024-10-13 17:45:13 -0700

    build game as dll

--- testbed/app.zig
+++ testbed/app.zig
@@ -1,9 +1,28 @@
-pub usingnamespace @import("entrypoint");
+const entrypoint = @import("entrypoint");
+
+pub const main = entrypoint.main;
+
+export fn init(engine: *core.Fracture) ?*anyopaque {
+    return game.init(engine);
+}
+
+export fn deinit(engine: *core.Fracture, game_state: *anyopaque) void {
+    game.deinit(engine, game_state);
+}
+
+export fn update(engine: *core.Fracture, game_state: *anyopaque) bool {
+    return game.update(engine, game_state);
+}
+
+export fn render(engine: *core.Fracture, game_state: *anyopaque) bool {
+    return game.render(engine, game_state);
+}
+
+export fn on_resize(engine: *core.Fracture, game_state: *anyopaque, width: u32, height: u32) void {
+    return game.on_resize(engine, game_state, width, height);
+}
+
 pub const config = @import("config.zig");
-
-// pub fn start(gpa: std.mem.Allocator) void {
-//     // If you need some startup code here.
-//     _ = gpa;
-// }
-
-const std = @import("std");
+const builtin = @import("builtin");
+const core = @import("fr_core");
+const game = @import("game.zig");
